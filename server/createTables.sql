aeda7d7b4a1ef9edf282f49e5f8859010cb78ee9
Celeste Yang <1123438490@qq.com>
2025-07-28 09:23:58 +0000

    add sql script and connect to database

---
+++ server/createTables.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS portfolio;
+USE portfolio;
+
+CREATE TABLE stock_currency (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ticker VARCHAR(20) NOT NULL,
+    name VARCHAR(100),
+    price DECIMAL(18, 4) ,
+    datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    asset_type ENUM('stock', 'currency') NOT NULL,
+    UNIQUE KEY unique_ticker_datetime (ticker, datetime)
+);
+
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    ticker VARCHAR(20) NOT NULL,
+    type ENUM('BUY', 'SELL') NOT NULL,
+    quantity DECIMAL(18, 4) NOT NULL,
+    price DECIMAL(18, 4) NOT NULL,
+    datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    asset_type ENUM('stock', 'currency') NOT NULL
+);
+
+CREATE TABLE portfolio (
+  ticker VARCHAR(20) PRIMARY KEY,   
+  quantity DECIMAL(18, 4) NOT NULL, 
+  avg_price DECIMAL(18, 4),   
+  asset_type ENUM('stock', 'currency') NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+);
+
+
+
