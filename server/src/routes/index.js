29c668b20c2157a94230861c2dc777262cc28840
LENOVO <983272259@qq.com>
2025-07-29 10:18:43 +0800

    add fetch all price

--- server/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { example } from '../controllers/index.js'; // 注意加 `.js`
-
-const router = express.Router();
-
-router.get('/example', example);
-
-export default router;
