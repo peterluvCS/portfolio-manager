f62ed071352718a4be0466de727c6a302426c7b0
Wenfei <wftan@berkeley.edu>
2025-07-29 15:14:57 +0800

    Update app.js: remove index.js dependency and register all routes directly

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import routes from './routes/index.js';
 import cors from 'cors';
 import tradeRoutes from './routes/trade.js';
 import priceRouter from './routes/priceRouter.js'; // Adjust the import path as necessary
-import swaggerUi from 'swagger-ui-express';
 import portfolioRoutes from './routes/portfolio.js';
+import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-
 
 const app = express();
 const PORT = process.env.PORT || 8081;
@@ -24,12 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/api/price', priceRouter);
-
-app.use('/api', routes);
+app.use('/api/portfolio', portfolioRoutes);
 app.use('/api/trade', tradeRoutes);
 
-app.use('/portfolio', portfolioRoutes);
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on /http://localhost:${PORT}`);
 });
--- server/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import portfolioRoutes from './portfolio.js';
-
-const router = express.Router();
-
-// router.get('/example', example);
-router.use('/portfolio', portfolioRoutes);
-
-export default router;
